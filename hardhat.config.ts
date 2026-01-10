2577f87ed0bd1137cbfd7f2bf3e7d7291cb2a6fc
Christopher Banwell <43727207+cbanwell@users.noreply.github.com>
2026-01-10 08:30:45 -0800

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "hardhat/config";
 
 export default defineConfig({
-  solidity: {
-    version: "0.8.28",
-  },
+    solidity: {
+        version: "0.8.28",
+    },
+
+    networks: {
+        supraTestnet: {
+            type: "http",
+            url: "https://rpc-testnet.supra.com",
+            accounts: process.env.DEPLOYER_PRIVATE_KEY
+                ? [process.env.DEPLOYER_PRIVATE_KEY]
+                : [],
+        },
+    },
 });
